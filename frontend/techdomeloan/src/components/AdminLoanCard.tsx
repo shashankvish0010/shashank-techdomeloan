217705e8f083001f55b8718623f6d4ab3fc340c5
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-12-01 18:48:12 +0530

    Bug:fixed

--- frontend/techdomeloan/src/components/AdminLoanCard.tsx
+++ frontend/techdomeloan/src/components/AdminLoanCard.tsx
@@ -8,21 +8,21 @@ const AdminLoanCard: React.FC<loansType> = (props) => {
   return (
     <div className="bg-blue-600 w-[85vw] text-white flex md:flex-row flex-col items-center justify-center gap-5 rounded-xl shadow-xl border p-3">
       <div className="title md:text-xl text-lg h-max md:w-[35vw] w-[70vw] flex flex-col items-start justify-around gap-3 p-3">
-        <span>
+        <span className="h-max w-[80%]">
           Applicant: {props.firstname} {props.lastname}
         </span>
-        <span>Email: {props.user_email}</span>
-        <span>Loan Amount: {props.loan_amount}</span>
-        <span>Term Duration: {props.term_duration}</span>
+        <span className="h-max w-[80%]">Email: {props.user_email}</span>
+        <span className="h-max w-[80%]">Loan Amount: {props.loan_amount}</span>
+        <span className="h-max w-[80%]">
+          Term Duration: {props.term_duration}
+        </span>
       </div>
       <div className="text-xl title h-[100%] md:w-[35vw] w-[70vw] flex flex-col items-center justify-around gap-3 p-3">
         <span>Loan Status</span>
-        <span>{props.loan_status}</span>
+        <span className="h-max w-[80%]">{props.loan_status}</span>
         {props.loan_status == "PENDING" ? (
           <span
             onClick={() => {
-              console.log(props.loanid);
-
               adminContext?.approveLoan(props.loanid);
             }}
             className="cursor-pointer rounded-xl h-max w-max p-3 bg-slate-100 text-blue-600"
